866d72d86b58be52efdbb010e96ff84e459ebec6
R K <blah@eample.com>
2019-05-15 12:25:26 +0200

    Updates

---
+++ precache-manifest.870a7737815bcfcf945b2d6022206be8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09022d73bf2cac46d0ad",
+    "url": "/react-crud-localstorage/static/js/runtime~main.09022d73.js"
+  },
+  {
+    "revision": "58b0c442f3937caedb9a",
+    "url": "/react-crud-localstorage/static/js/main.58b0c442.chunk.js"
+  },
+  {
+    "revision": "8f335e074d48b18d3698",
+    "url": "/react-crud-localstorage/static/js/1.8f335e07.chunk.js"
+  },
+  {
+    "revision": "58b0c442f3937caedb9a",
+    "url": "/react-crud-localstorage/static/css/main.e37cccfb.chunk.css"
+  },
+  {
+    "revision": "587576ed82637aaa719685ab44f71acd",
+    "url": "/react-crud-localstorage/index.html"
+  }
+];
